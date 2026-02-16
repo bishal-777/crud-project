5a4686faa205ec6bd779a41163a77e782fd1a5a7
bishal <bishal777giri@gmail.com>
2026-02-16 11:44:45 +0545

    completed project

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import BlogModel
 # Register your models here.
+
+@admin.register(BlogModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'last_updated', 'date_of_post')
+    list_filter = ('date_of_post', 'author')
+    search_fields = ('title', 'author')
